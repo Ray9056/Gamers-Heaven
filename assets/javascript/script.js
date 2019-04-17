f12285499753df95c45b8bfdee221bdd765651e0
Jerne <Surajpatel527@gmail.com>
2019-04-17 15:20:28 -0400

    Changelog: Fixed some background sizing bugs

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,7 +1,9 @@
 //Initial array of games	
 $(document).ready(function() {
 
-    var gameGif = ["World Of Warcraft", "Fortnite", "Call of Duty", "God Of War", "League of Legends", "Assassin's Creed", "Minecraft", "Devil May Cry 5", "Super Smash Bros.", "Overwatch", "Grand Theft Auto", "The Legend of Zelda", "Mortal Kombat"];	
+    var gameGif = ["World Of Warcraft", "Fortnite", "Call of Duty", "God Of War", "League of Legends", 
+    "Assassin's Creed", "Minecraft", "Devil May Cry 5", "Super Smash Bros.", "Overwatch", "Grand Theft Auto", 
+    "The Legend of Zelda", "Mortal Kombat"];	
   
     //  create gameGif array buttons
     function renderButtons(){
@@ -54,19 +56,19 @@ $(document).ready(function() {
   
   }); // done response
   
-          //function to stop and animate gifs
-          function playGif() { 
-                      var state = $(this).attr('data-state');
+      //function to stop and animate gifs
+      function playGif() { 
+                  var state = $(this).attr('data-state');
 
-                      if (state == 'still'){
-                       $(this).attr('src', $(this).data('animate'));
-                        $(this).attr('data-state', 'animate');
-                   } else{
-                       $(this).attr('src', $(this).data('still'));
-                       $(this).attr('data-state', 'still');
-                      }
-  
-                  } //end of on click function
+                  if (state == 'still'){
+                    $(this).attr('src', $(this).data('animate'));
+                    $(this).attr('data-state', 'animate');
+                } else{
+                    $(this).attr('src', $(this).data('still'));
+                    $(this).attr('data-state', 'still');
+                  }
+
+            } //end of on click function
   
         }); //end of document on click 
   
